b910a569f7f59dd151ea02d397efb7776e26beee
Brenda-CW <factotum4rays@gmail.com>
2022-02-14 17:42:04 -0700

    Desktop styles & Button completed

--- js/script.js
+++ js/script.js
@@ -3,20 +3,16 @@
 // share button also needs to remove hide class from social class
 
 const openShareBtn = document.getElementById("share--btn");
-const closeShareBtn = document.querySelector(".user-btn");
+const closeShareBtn = document.querySelector(".user--btn");
 const userBlock = document.querySelector(".user");
-const socialBlock = document.querySelector(".social--bar");
+const socialBlock = document.querySelector(".social");
 
-function share() {
-    console.log({openShareBtn});
-    console.log("share button clicked!")
-    userBlock.classList.add("hide");
+openShareBtn.addEventListener('click', () => {
     socialBlock.classList.remove("hide");
-}
+    userBlock.classList.add("hide");
+});
 
-function user() {
+closeShareBtn.addEventListener('click', () => {
     socialBlock.classList.add("hide");
     userBlock.classList.remove("hide");
-}
-
-openShareBtn.addEventListener('click', share());
+});
